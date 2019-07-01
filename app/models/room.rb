f710af153f572689263da9dc9fd99c38bc45625f
ryanjkirkland <ryankirklandga@gmail.com>
2019-06-30 21:01:13 -0600

    configured activestorage, connect rooms in a one to many relationship for photo uploads, styled photo upload page to confirm and preview all uploaded photos

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,9 +1,11 @@
 class Room < ApplicationRecord
   belongs_to :user
+  has_many_attached :images
 
   validates :home_type, presence: true
   validates :room_type, presence: true
   validates :accomodate, presence: true
   validates :bed_room, presence: true
   validates :bath_room, presence: true
+  validates :images, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
       get 'amenities'
       get 'location'
     end
+    resources :images, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
