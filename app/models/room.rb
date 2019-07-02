78f880963a852f89e926cfdbffda8fea093047fb
ryanjkirkland <ryankirklandga@gmail.com>
2019-07-01 20:08:42 -0600

    create view of all created rooms for a user

--- app/models/room.rb
+++ app/models/room.rb
@@ -7,4 +7,12 @@ class Room < ApplicationRecord
   validates :accomodate, presence: true
   validates :bed_room, presence: true
   validates :bath_room, presence: true
+
+  def cover_photo
+  	if self.images.length > 0
+  		self.images[0]
+	else
+		"blank.jpg"
+	end
+  end
 end
